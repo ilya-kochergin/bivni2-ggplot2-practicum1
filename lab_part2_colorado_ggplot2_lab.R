1b1d5119ce7eea8239ae0f3ed39bc8ee07f57d57
ilya-kochergin <ikochergin@gmail.com>
2015-10-18 12:55:55 +0300

    bar plot almost done.

--- lab_part2_colorado_ggplot2_lab.R
+++ lab_part2_colorado_ggplot2_lab.R
@@ -8,4 +8,23 @@ film_death_counts <- read.csv(file="film_death_counts.csv",header = T)
 library(ggplot2)
 
 head(film_death_counts)
+film_death_counts 
+ggplot(data=film_death_counts,aes(Year,
+                                  IMDB_Rating,
+                                  col=MPAA_Rating)) +
+         geom_point()+ylab("Average IMDB Raiting")
 
+
+# график 4
+
+
+film_death_counts$death_per_minute <-  
+     film_death_counts$Body_Count/film_death_counts$Length_Minutes
+film_death_counts$TitleAndYear <- sprintf("%s (%s)",
+  film_death_counts$Film,film_death_counts$Year)
+
+reordered_data <-  film_death_counts[order(film_death_counts$death_per_minute,
+                                                 decreasing = TRUE)[1:20],]
+
+ggplot(data=reordered_data,aes(x=TitleAndYear,y=death_per_minute,fill=MPAA_Rating),ordered=T) + coord_flip()+ geom_bar(stat="identity",position = "dodge")
+View(reordered_data)
