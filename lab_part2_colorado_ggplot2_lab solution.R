07d0e93121d57e6fcfc6d3d476e335dae6caad79
ilya-kochergin <ikochergin@gmail.com>
2015-10-22 15:48:57 +0300

    lab2_part2_colorado_ggplot2_lab.R: solution moved to separate file lab2_part2_colorado_ggplot2_lab_solution.R

---
+++ lab_part2_colorado_ggplot2_lab solution.R
@@ -0,0 +1,34 @@
+##################################################################
+### лабораторная работа ggplot2:  Часть вторая лабораторная работа
+### университета Колорадо 
+##################################################################
+
+film_death_counts <- read.csv(file="film_death_counts.csv",header = T)
+
+library(ggplot2)
+
+head(film_death_counts)
+film_death_counts 
+ggplot(data=film_death_counts,aes(Year,
+                                  IMDB_Rating,
+                                  col=MPAA_Rating)) +
+         geom_point()+ylab("Average IMDB Raiting")
+
+
+# график 4
+
+
+film_death_counts$death_per_minute <-  
+     film_death_counts$Body_Count/film_death_counts$Length_Minutes
+film_death_counts$TitleAndYear <- sprintf("%s (%s)",
+  film_death_counts$Film,film_death_counts$Year)
+
+top20 <-  film_death_counts[order(film_death_counts$death_per_minute,
+                                                 decreasing = TRUE)[1:20],]
+top20$TitleAndYear <-  reorder(top20$TitleAndYear,top20$death_per_minute)
+
+ggplot(data=top20,aes(x=TitleAndYear,y=death_per_minute,fill=MPAA_Rating),ordered=T) +
+   coord_flip()+ geom_bar(stat="identity") + 
+  xlab("Film title and year")+ ylab("Смертей в секунду")
+# device cairo_pdf correctly work with russian letter
+ggsave(filename = "plot4.pdf",device=cairo_pdf)
--- lab_part2_colorado_ggplot2_lab.R
+++ lab_part2_colorado_ggplot2_lab.R
@@ -8,27 +8,13 @@ film_death_counts <- read.csv(file="film_death_counts.csv",header = T)
 library(ggplot2)
 
 head(film_death_counts)
-film_death_counts 
-ggplot(data=film_death_counts,aes(Year,
-                                  IMDB_Rating,
-                                  col=MPAA_Rating)) +
-         geom_point()+ylab("Average IMDB Raiting")
 
+
+# график 1
+
+
+# график 2
+
+# график 3
 
 # график 4
-
-
-film_death_counts$death_per_minute <-  
-     film_death_counts$Body_Count/film_death_counts$Length_Minutes
-film_death_counts$TitleAndYear <- sprintf("%s (%s)",
-  film_death_counts$Film,film_death_counts$Year)
-
-top20 <-  film_death_counts[order(film_death_counts$death_per_minute,
-                                                 decreasing = TRUE)[1:20],]
-top20$TitleAndYear <-  reorder(top20$TitleAndYear,top20$death_per_minute)
-
-ggplot(data=top20,aes(x=TitleAndYear,y=death_per_minute,fill=MPAA_Rating),ordered=T) +
-   coord_flip()+ geom_bar(stat="identity") + 
-  xlab("Film title and year")+ ylab("Смертей в секунду")
-# device cairo_pdf correctly work with russian letter
-ggsave(filename = "plot4.pdf",device=cairo_pdf)
